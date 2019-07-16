f37fb1a4ef207b4e49aac065206e206ef06c954f
Samuel T <ssamt1142@gmail.com>
2019-07-16 20:44:41 +0000

    Automatically backed up by Learn

--- spec/controllers/pets_controller_spec.rb
+++ spec/controllers/pets_controller_spec.rb
@@ -78,7 +78,7 @@ describe "Pets Controller" do
       # binding.pry
       visit "/pets/#{@pet.id}/edit"
       expect(page).to have_field('pet_name')
-      expect(page.has_checked_field?(@owner.name)).to eq(true)
+      expect(page.has_checked_field?(@owner.id)).to eq(true)
       expect(page).to have_field('owner[name]')
     end
 
